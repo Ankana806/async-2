9650e7b7e90af9f532b6c0db3db793350bc83c00
Ankana806 <85381913+Ankana806@users.noreply.github.com>
2021-06-25 23:15:17 +0530

    Add files via upload

---
+++ ice-cream-await-3.js
@@ -0,0 +1,46 @@
+let stocks={
+  fruits:["strawberry","grapes","banana","apple"],
+  liquid:["water","ice"],
+  holder:["cone","cup","stick"],
+  toppings:["chocolate","peanuts"]
+};
+
+
+let is_shop_open=true;
+function time(ms){
+  return new Promise((resolve,reject)=>{
+    if(is_shop_open){
+      setTimeout(resolve,ms)
+    }
+    else{
+      reject(console.log("shop is closed"));
+    }
+  });
+}
+async function kitchen(){
+  try{
+    await time(2000)
+    console.log(`${stocks.fruits[0]} was selected`);
+    await time(0000)
+    console.log("start the production");
+    await time(2000)
+    console.log("cut the fruit");
+    await time(1000)
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+    await time(1000)
+    console.log("start the machine");
+    await time(2000)
+    console.log(`${stocks.holder[0]} was added`);
+    await time(3000)
+    console.log(`${stocks.toppings[0]} was added`);
+    await time(2000)
+    console.log("ice cream served");
+  }
+  catch(error){
+    console.log("customer left",error)
+}
+  finally{
+    console.log("day ended shop is closed")
+  }
+}
+kitchen();
